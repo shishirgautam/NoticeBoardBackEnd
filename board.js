d01d06a6ade3b5da26200ac9a60de12c6a813ae0
shishirgautam <gautamshishir2@gmail.com>
2020-01-26 20:54:49 +0545

    [#5]registrator user validation

--- board.js
+++ board.js
@@ -3,9 +3,13 @@ var app = express();
 var bodyParser = require('body-parser');
 var userController = require('./controllers/User_controller.js')
 app.use(bodyParser.urlencoded({extended:true}))
-app.get('/*', function(req,res){
-    res.end('welcome to our new project run')
-});
+// app.get('/*', function(req,res){
+//     res.end('welcome to our new project run')
+//});
+// app.post('/registration', function(req,res){
+//     res.end('welcome to our new project registration')
+// });
+
 
 app.post('/registration',userController.validation,userController.hashGen,userController.registerUser )
 app.listen(8888);
